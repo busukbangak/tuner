6ddebbf87768acff66a68df74b9419ffef154b58
awanali <aliim.wanali@gmail.com>
2025-08-08 12:25:29 +0200

    Add basic frequency detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+import { useEffect } from "react";
+import autoCorrelate, { getAudioData, setupMicrophone } from "./shared/utils";
 
 function App() {
 
+  useEffect(() => {
+    const audioContext = new window.AudioContext();
+    const analyser = audioContext.createAnalyser();
+    const buffer = new Float32Array(analyser.fftSize);
+    let pitchDetectionIntervalID: number;
+
+    if (audioContext.state === "suspended") {
+      audioContext.resume();
+    }
+
+    const startPitchDetection = async () => {
+      const mediaStream = await setupMicrophone(); // Setup Microphone
+      const mediaSource = audioContext.createMediaStreamSource(mediaStream); // Stream all the audio from microphone into audioContext
+      mediaSource.connect(analyser); // Connect the audio data to an analyser
+
+      pitchDetectionIntervalID = setInterval(() => {
+        const audioBufferData = getAudioData(analyser, buffer);
+        console.log("Buffer:", audioBufferData);
+        const frequency = autoCorrelate(audioBufferData, audioContext.sampleRate)
+        console.log("Frequency:", frequency);
+      }, 1000);
+    };
+
+    startPitchDetection();
+
+    return () => {
+      if (pitchDetectionIntervalID) clearInterval(pitchDetectionIntervalID);
+      audioContext.close();
+    }
+  }, [])
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline italic text-red-400">Hello World</h1>
+      <button>asd</button>
     </>
   )
 }
---
+++ src/shared/utils.ts
@@ -0,0 +1,65 @@
+export const setupMicrophone = async () => {
+    const microphone = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false
+    });
+    return microphone;
+}
+
+export const getAudioData = (analyser: AnalyserNode, buffer: Float32Array) => {
+    analyser.getFloatTimeDomainData(buffer);
+    return buffer;
+}
+
+// Info: Copied from here https://github.com/zplata/tuner-app/blob/main/autocorrelate.js
+export default function autoCorrelate(buffer: Float32Array<ArrayBuffer>, sampleRate: number) {
+    let SIZE = buffer.length;
+    let rms = 0;
+
+    for (let i = 0; i < SIZE; i++) {
+        const val = buffer[i];
+        rms += val * val;
+    }
+    rms = Math.sqrt(rms / SIZE);
+    if (rms < 0.01)
+        // not enough signal
+        return -1;
+
+    let r1 = 0, r2 = SIZE - 1;
+    const thres = 0.2;
+    for (let i = 0; i < SIZE / 2; i++)
+        if (Math.abs(buffer[i]) < thres) {
+            r1 = i;
+            break;
+        }
+    for (let i = 1; i < SIZE / 2; i++)
+        if (Math.abs(buffer[SIZE - i]) < thres) {
+            r2 = SIZE - i;
+            break;
+        }
+
+    buffer = buffer.slice(r1, r2);
+    SIZE = buffer.length;
+
+    const c = new Array(SIZE).fill(0);
+    for (let i = 0; i < SIZE; i++)
+        for (let j = 0; j < SIZE - i; j++) c[i] = c[i] + buffer[j] * buffer[j + i];
+
+    let d = 0;
+    while (c[d] > c[d + 1]) d++;
+    let maxval = -1, maxpos = -1;
+    for (let i = d; i < SIZE; i++) {
+        if (c[i] > maxval) {
+            maxval = c[i];
+            maxpos = i;
+        }
+    }
+    let T0 = maxpos;
+
+    const x1 = c[T0 - 1], x2 = c[T0], x3 = c[T0 + 1];
+    const a = (x1 + x3 - 2 * x2) / 2;
+    const b = (x3 - x1) / 2;
+    if (a) T0 = T0 - b / (2 * a);
+
+    return sampleRate / T0;
+}
